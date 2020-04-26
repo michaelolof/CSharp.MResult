9a3d60e8b087feb858e77609dc7806f7e2a10ea1
michaelolof <myqrrlolof@yahoo.com>
2020-04-26 11:47:05 +0100

    added test cases for other methods in the Result class

---
+++ MResult.Tests/src/Result_GetErrOrDefault.tests.cs
@@ -0,0 +1,45 @@
+using System;
+using Xunit;
+
+namespace Michaelolof.MResult.Tests
+{
+
+  public class Result_GetErrOrDefault__Tests
+  {
+
+    [Fact]
+    public void Should_Return_Err_When_Result_Is_Err()
+    {
+      // Arrange
+      var err = new Exception("Nothing");
+      var defaultErr = new Exception("Default");
+      var result = Result<int,Exception>.Err( err );
+      
+      // Act
+      var ex = result.GetErrOrDefault( defaultErr );
+    
+      // Assert
+      Assert.True( ex is Exception );
+      Assert.True( ex.Message == err.Message );
+    }
+
+
+    [Fact]
+    public void Should_Return_Default_Err_When_Result_Is_Ok()
+    {
+     // Arrange
+      var defaultErr = new Exception("Default");
+      var result = Result<int,Exception>.Ok( 20 );
+      
+      // Act
+      var ex = result.GetErrOrDefault( defaultErr );
+    
+      // Assert
+      Assert.True( ex is Exception );
+      Assert.True( ex.Message == defaultErr.Message );
+    }
+
+  }
+
+
+}
---
+++ MResult.Tests/src/Result_GetValueAndErr.tests.cs
@@ -0,0 +1,66 @@
+using System;
+using Xunit;
+
+namespace Michaelolof.MResult.Tests
+{
+
+  public class Result_GetValueAndErr__Tests
+  {
+
+    [Fact]
+    public void Should_Return_A_Tuple_Of_Value_And_Null_When_Result_Is_Ok()
+    {
+      var result = Result<int, Exception>.Ok( 20 );
+
+      var (val, err) = result.GetValueAndErr();
+
+      Assert.True( val == 20 );
+      Assert.True( err == null );
+    }
+
+    [Fact]
+    public void Should_Return_A_Tuple_Of_Zero_And_Err_When_Result_Is_Err_And_Value_Is_An_Integer()
+    {
+      var result = Result<int, Exception>.Err( new Exception("Nothing") );
+
+      var (val, err) = result.GetValueAndErr();
+
+      Assert.True( val == 0 );
+      Assert.True( err is Exception );
+      Assert.True( err.Message == "Nothing" );
+    }
+
+    [Fact]
+    public void Should_Return_A_Tuple_Of_Null_And_Exception_When_Result_Is_Err_And_Value_Is_A_Reference()
+    {
+      var result = Result<Tokens,Exception>.Err( new Exception("Nothing") );
+
+      var (val, err) = result.GetValueAndErr();
+
+      Assert.True( val == null );
+      Assert.True( err is Exception );
+      Assert.True( err.Message == "Nothing" );
+    }
+
+    [Fact]
+    public void Should_Return_A_Tuple_Of_Empty_Struct_And_Exception_When_Result_Is_Err_And_Value_Is_Struct()
+    {
+      var result = Result<Struct, Exception>.Err( new Exception("Nothing") );
+
+      var (val, err) = result.GetValueAndErr();
+
+      Assert.True( val.Name == "" );
+      Assert.True( err is Exception );
+      Assert.True( err.Message == "Nothing" );
+    }
+
+    public class Tokens {}
+
+    public struct Struct {
+      public string Name {get;}
+    }
+
+
+  }
+
+}
